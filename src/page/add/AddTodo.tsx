c2f4f3f3acd7289121f345ccea21f45b990655fc
siyu.chen <siyu.chen@thoughtworks.com>
2023-03-22 14:15:50 +0800

    [AddTodo]Feat: show add-input.

---
+++ src/page/add/AddTodo.tsx
@@ -0,0 +1,23 @@
+import React, { ChangeEvent, useState } from 'react';
+
+const AddTodo: React.FC = () => {
+	const [inputVal, setInputVal] = useState<string>('');
+	const handleChangeInput = (event: ChangeEvent<HTMLInputElement>): void => {
+		setInputVal(event.target.value);
+	};
+
+	return (
+		<>
+			<div className="add">
+				<input
+					value={inputVal}
+					type="text"
+					placeholder="Enter your todo item"
+					onChange={handleChangeInput}
+				/>
+			</div>
+		</>
+	);
+};
+
+export default AddTodo;
