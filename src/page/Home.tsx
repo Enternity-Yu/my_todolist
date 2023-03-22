75f5fb882a557151a909881b6404ad9f52b241a3
siyu.chen <siyu.chen@thoughtworks.com>
2023-03-22 13:24:42 +0800

    [Home]Feat: show title.

---
+++ src/page/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = () => {
+	return (
+		<div className="home" data-testid="home-element">
+			<div className="title">
+				<h1>Welcome To The Todo-List</h1>
+				<h3>Use this to manage your work and life, easily!</h3>
+			</div>
+		</div>
+	);
+};
+
+export default Home;
