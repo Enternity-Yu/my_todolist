f0bdb971b27f82ee96f2bbc64e0df20ea2b1d04b
siyu.chen <siyu.chen@thoughtworks.com>
2023-03-22 14:15:37 +0800

    [AddTodo]Test: show add-input.

---
+++ src/page/add/AddTodo.test.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import AddTodo from './AddTodo';
+
+describe('render AddTodo', () => {
+	beforeEach(() => render(<AddTodo />));
+
+	test('should show add-input correctly', () => {
+		const element = screen.getByRole(/textbox/i);
+
+		expect(element).toBeInTheDocument();
+		expect(element).toHaveAttribute('placeholder', 'Enter your todo item');
+	});
+});
