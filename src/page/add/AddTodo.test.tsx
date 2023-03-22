4492c5dedc778d2ac63239ba409839fd14c11c17
siyu.chen <siyu.chen@thoughtworks.com>
2023-03-22 16:27:47 +0800

    [AddTodo]Test: test onChange event of add-input.

--- src/page/add/AddTodo.test.tsx
+++ src/page/add/AddTodo.test.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import AddTodo from './AddTodo';
 
 describe('render AddTodo', () => {
 	beforeEach(() => render(<AddTodo />));
 
 	test('should show add-input correctly', () => {
-		const element = screen.getByRole(/textbox/i);
+		const element: HTMLInputElement = screen.getByRole(/textbox/i);
 
 		expect(element).toBeInTheDocument();
 		expect(element).toHaveAttribute('placeholder', 'Enter your todo item.');
+	});
+
+	test('should test onChange event of add-input', () => {
+		const element: HTMLInputElement = screen.getByRole(
+			/textbox/i
+		) as HTMLInputElement;
+		fireEvent.change(element, { target: { value: 'task test' } });
+
+		expect(element.value).toBe('task test');
 	});
 });
