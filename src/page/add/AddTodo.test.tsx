5e1bbb08695566c095a5673d0ee5093993286198
siyu.chen <siyu.chen@thoughtworks.com>
2023-03-22 19:10:33 +0800

    [AddTodo]Test: test className of add-input when isShowError is false .

--- src/page/add/AddTodo.test.tsx
+++ src/page/add/AddTodo.test.tsx
@@ -3,21 +3,24 @@ import { fireEvent, render, screen } from '@testing-library/react';
 import AddTodo from './AddTodo';
 
 describe('render AddTodo', () => {
-	beforeEach(() => render(<AddTodo />));
+	let element: HTMLInputElement;
+	beforeEach(() => {
+		render(<AddTodo />);
+		element = screen.getByRole(/textbox/i) as HTMLInputElement;
+	});
 
 	test('should show add-input correctly', () => {
-		const element: HTMLInputElement = screen.getByRole(/textbox/i);
-
 		expect(element).toBeInTheDocument();
 		expect(element).toHaveAttribute('placeholder', 'Enter your todo item.');
 	});
 
-	test('should test onChange event of add-input', () => {
-		const element: HTMLInputElement = screen.getByRole(
-			/textbox/i
-		) as HTMLInputElement;
+	test('test onChange event of add-input', () => {
 		fireEvent.change(element, { target: { value: 'task test' } });
 
 		expect(element.value).toBe('task test');
+	});
+
+	test('should have add-input class when isShowError is false ', () => {
+		expect(element).toHaveClass('add-input');
 	});
 });
