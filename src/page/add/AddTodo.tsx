ca25c663e343343b874b7a5f0db2c7881761e561
siyu.chen <siyu.chen@thoughtworks.com>
2023-03-23 13:37:48 +0800

    [AddTodo]Feat: complete addtodo.

--- src/page/add/AddTodo.tsx
+++ src/page/add/AddTodo.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
-import { TagItemObj } from '../../type';
+import React, { ChangeEvent, useEffect, useRef, useState } from 'react';
+import { ulid } from 'ulid';
+import { TagItemObj, TaskItemObj } from '../../type';
 import './AddTodo.scss';
 
 const AddTodo: React.FC = () => {
@@ -10,7 +11,69 @@ const AddTodo: React.FC = () => {
 	};
 
 	const [inputVal, setInputVal] = useState<string>('');
+	const [taskName, setTaskName] = useState<string>('');
 	const [taskTags, setTaskTags] = useState<TagItemObj>({ ...initTaskTags });
+	const [selectedTags, setSelectedTags] = useState<string[]>(['']);
+	const [newTask, setNewTask] = useState<TaskItemObj>();
+	const [isShowError, setIsShowError] = useState<boolean>(false);
+	const [errorMessage, setErrorMessage] = useState<string>('');
+
+	const createFirstUpload = useRef(true);
+	const addFirstUpload = useRef(true);
+
+	useEffect(() => {
+		if (createFirstUpload.current) {
+			createFirstUpload.current = false;
+			return;
+		}
+		setNewTaskItemAndResetValue();
+	}, [taskName, selectedTags]);
+
+	useEffect(() => {
+		if (addFirstUpload.current) {
+			addFirstUpload.current = false;
+			return;
+		}
+		createNewTaskToList();
+	}, [newTask]);
+
+	const createNewTaskToList = (): void => {
+		if (!newTask?.name) {
+			setIsShowError(true);
+			setErrorMessage('Please enter the correct task content.');
+			return;
+		}
+		if (!newTask?.tags.length) {
+			setIsShowError(true);
+			setErrorMessage('Please select the task-tag.');
+			return;
+		}
+	};
+
+	const setNewTaskItemAndResetValue = (): void => {
+		setNewTask({
+			id: ulid(),
+			name: taskName,
+			tags: selectedTags,
+			isFinished: false,
+		});
+		setInputVal('');
+		setTaskTags(initTaskTags);
+	};
+
+	const handleCreateTask = (): void => {
+		setTaskName(inputVal.trim());
+		setSelectedTags(Object.keys(taskTags).filter((key) => taskTags[key]));
+	};
+
+	const handleClickTaskTag = (tagKey: string, isSelected: boolean): void => {
+		setTaskTags((pre: TagItemObj) => ({ ...pre, [tagKey]: !isSelected }));
+	};
+
+	const handleChangeInput = (event: ChangeEvent<HTMLInputElement>): void => {
+		setInputVal(event.target.value);
+		setIsShowError(false);
+	};
 
 	return (
 		<>
@@ -20,10 +83,16 @@ const AddTodo: React.FC = () => {
 					value={inputVal}
 					type="text"
 					placeholder="Enter your todo item."
+					onChange={handleChangeInput}
 				/>
-				<button className="add-btn">
+				<button className="add-btn" onClick={handleCreateTask}>
 					<i className="bi-plus-lg"></i>
 				</button>
+				<span
+					className={`error-hint ${isShowError ? 'error-hint-display' : null}`}
+				>
+					{errorMessage}
+				</span>
 			</div>
 			<div className="select-task-tags">
 				{Object.keys(taskTags).map((tagItem) => (
@@ -32,6 +101,7 @@ const AddTodo: React.FC = () => {
 						className={`task-tag-item ${
 							taskTags[tagItem] ? 'task-tag-item-active' : null
 						}`}
+						onClick={() => handleClickTaskTag(tagItem, taskTags[tagItem])}
 					>
 						{tagItem}
 					</span>
