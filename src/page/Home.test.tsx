d39f2df0f21d4f1e54420dcfbc6e89e0c55f4c71
siyu.chen <siyu.chen@thoughtworks.com>
2023-03-23 13:37:07 +0800

    [Home]Fix.

--- src/page/Home.test.tsx
+++ src/page/Home.test.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import Home from './Home';
 
 describe('render Home', () => {
-	render(<Home />);
 
 	it('should show TodoList title correctly', () => {
+		render(<Home/>);
+
 		const element = screen.getAllByRole('heading');
 
 		expect(element[0]).toHaveTextContent('Welcome To The Todo-List');
-		expect(element[1]).toHaveTextContent(
-			'Use this to manage your work and life, easily!'
-		);
+		expect(element[1]).toHaveTextContent('Use this to manage your work and life, easily!');
 	});
 
 	it('should render Add-Todo component correctly', () => {
+		render(<Home/>);
+
 		const element = screen.getByTestId('add-todo-element');
 
 		expect(element).toBeInTheDocument();
