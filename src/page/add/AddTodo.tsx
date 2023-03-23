190aa5616eef99bcfb4239af066f1a7b0d156847
siyu.chen <siyu.chen@thoughtworks.com>
2023-03-23 11:03:35 +0800

    [AddTodo]Feat: implemented AddTodo ui.

--- src/page/add/AddTodo.tsx
+++ src/page/add/AddTodo.tsx
@@ -1,14 +1,16 @@
-import React, { ChangeEvent, useState } from 'react';
+import React, { useState } from 'react';
+import { TagItemObj } from '../../type';
 import './AddTodo.scss';
 
 const AddTodo: React.FC = () => {
-	const [inputVal, setInputVal] = useState<string>('');
-	const [isShowError, setIsShowError] = React.useState<boolean>(false);
-
-	const handleChangeInput = (event: ChangeEvent<HTMLInputElement>): void => {
-		setInputVal(event.target.value);
-		setIsShowError(false);
+	const initTaskTags: TagItemObj = {
+		study: false,
+		work: false,
+		life: false,
 	};
+
+	const [inputVal, setInputVal] = useState<string>('');
+	const [taskTags, setTaskTags] = useState<TagItemObj>({ ...initTaskTags });
 
 	return (
 		<>
@@ -18,8 +20,22 @@ const AddTodo: React.FC = () => {
 					value={inputVal}
 					type="text"
 					placeholder="Enter your todo item."
-					onChange={handleChangeInput}
 				/>
+				<button className="add-btn">
+					<i className="bi-plus-lg"></i>
+				</button>
+			</div>
+			<div className="select-task-tags">
+				{Object.keys(taskTags).map((tagItem) => (
+					<span
+						key={tagItem}
+						className={`task-tag-item ${
+							taskTags[tagItem] ? 'task-tag-item-active' : null
+						}`}
+					>
+						{tagItem}
+					</span>
+				))}
 			</div>
 		</>
 	);
---
+++ src/type.ts
@@ -0,0 +1,22 @@
+export type TaskItemObj = {
+    id: string,
+    name: string,
+    tags: string[],
+    isFinished: boolean
+}
+
+export type TagItemObj = {
+    [key: string]: boolean
+}
+
+export type ActionType = {
+    type: string,
+    task: TaskItemObj,
+    id: string,
+    name: string,
+    isFinished: boolean
+}
+
+export type EditFormValues = {
+    name: string;
+}
