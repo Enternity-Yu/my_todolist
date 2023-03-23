3db5ab9bc490c825ddd859f6598dcc4077250779
siyu.chen <siyu.chen@thoughtworks.com>
2023-03-23 17:12:50 +0800

    Config.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -6,14 +6,10 @@ describe('render App', () => {
 	beforeEach(() => render(<App />));
 
 	test('should render App correctly', () => {
-		const app_div_element = screen.getByTestId('app-div-element');
-
-		expect(app_div_element).toBeInTheDocument();
+		expect(screen.getByTestId('app-div-element')).toBeInTheDocument();
 	});
 
 	test('should render Home component correctly', () => {
-		const home_element = screen.getByTestId('home-element');
-
-		expect(home_element).toBeInTheDocument();
+		expect(screen.getByTestId('home-element')).toBeInTheDocument();
 	});
 });
