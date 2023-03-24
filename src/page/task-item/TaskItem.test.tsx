bc91e742bc9c40def1e0e03817ecdd3149b6f159
siyu.chen <siyu.chen@thoughtworks.com>
2023-03-24 11:23:07 +0800

    Test: column test tasks of TaskItem.

---
+++ src/page/task-item/TaskItem.test.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+describe('render TaskItem', () => {
+	describe('show task item', () => {
+		it('should show task name, tags and actions when add task successfully', () => {});
+	});
+
+	describe('check task', () => {
+		it('should not show checked task on Todo-List when check task', () => {});
+
+		it('should show checked task on Completed-List when check task', () => {});
+	});
+
+	describe('edit task', () => {
+		it('should show edit-prompt-box when click edit button', () => {});
+
+		it('should show old task name when click cancel', () => {});
+
+		it('should show new task name when enter new content and click OK', () => {});
+	});
+
+	describe('delete task', () => {
+		it('should show delete-prompt-box when click delete button', () => {});
+
+		it('should show this task on TO-DO-List when click cancel', () => {});
+
+		it('should not show this task when click Yes', () => {});
+	});
+});
