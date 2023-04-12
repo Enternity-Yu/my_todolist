870ec522875783dc520bd05e5fd2aacd8f067532
siyu.chen <siyu.chen@thoughtworks.com>
2023-04-12 10:15:41 +0800

    Type: add function type.

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useMemo } from 'react';
+import React, { useEffect, useState } from 'react';
 import AddTodo from './add/AddTodo';
 import TaskList from './task-list/TaskList';
 import { createTask, deleteTask, getTasks, updateTask } from '../api/tasks';
@@ -8,24 +8,24 @@ const Home: React.FC = () => {
 	const [taskList, setTaskList] = useState([]);
 	const [loading, setLoading] = useState(false);
 
-	const getAllTasks = async () => {
+	const getAllTasks = async (): Promise<void> => {
 		const res = await getTasks();
 		setTaskList(res);
 	};
 
-	const createNewTask = async (data: any) => {
+	const createNewTask = async (data: any): Promise<void> => {
 		setLoading(true);
 		await createTask(data);
 		setLoading(false);
 	};
 
-	const update = async (id: number, data: any) => {
+	const update = async (id: number, data: any): Promise<void> => {
 		setLoading(true);
 		await updateTask(id, data);
 		setLoading(false);
 	};
 
-	const remove = async (id: number) => {
+	const remove = async (id: number): Promise<void> => {
 		setLoading(true);
 		await deleteTask(id);
 		setLoading(false);
